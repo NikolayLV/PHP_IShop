d427f762675224b9329917b7a2d2f1190b599376
NikolayLV <Nikolayd2002@gmail.com>
2023-08-14 11:50:04 +0300

    Create routers.

--- public/index.php
+++ public/index.php
@@ -4,10 +4,8 @@ if (PHP_MAJOR_VERSION < 8) {
     die('Нужна версия PHP >= 8');
 }
 
-
-
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS(__DIR__) . '/functions.php';
+require_once CONFIG(__DIR__) . '/routes.php';
 
 new \wfm\App();
-
-throw new Exception('Возникла ошибочка');
---
+++ vendor/wfm/Router.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace wfm;
+
+
+class Router
+{
+
+    protected static array $routes = [];
+    protected static array $route = [];
+
+}
