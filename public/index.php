d44da8aa4555fe7c3fb55e33df6cfc2285df80f6
NikolayLV <Nikolayd2002@gmail.com>
2023-08-14 12:16:49 +0300

    Update routes.

--- public/index.php
+++ public/index.php
@@ -5,7 +5,9 @@ if (PHP_MAJOR_VERSION < 8) {
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
-require_once HELPERS(__DIR__) . '/functions.php';
-require_once CONFIG(__DIR__) . '/routes.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
+
+debug(\wfm\Router::getRoutes());
--- vendor/wfm/App.php
+++ vendor/wfm/App.php
@@ -11,9 +11,12 @@ class App
 
     public function __construct()
     {
+        $query = trim(urldecode($_SERVER['QUERY_STRING']), '/');
        new ErrorHandler();
         self::$app = Regisrty::getInstance();
         $this->getParams();
+        Router::dispatch($query);
+
     }
 
     protected function getParams()
@@ -25,5 +28,6 @@ class App
             }
         }
     }
+
 
 }
