504028d2f0e4d745af7c9ad9fff4f02f9a47d3b9
NikolayLV <Nikolayd2002@gmail.com>
2023-08-15 14:05:37 +0300

    Create controllers for Route.

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@
 use wfm\Router;
 
 Router::add('^admin/?$', ['controller' => 'Main', 'action' => 'index', 'admin_prefix' => 'admin']);
+
 Router::add('^admin/(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['admin_prefix' => 'admin']);
 
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
