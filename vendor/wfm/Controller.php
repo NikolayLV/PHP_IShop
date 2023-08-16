4579b1959827e6565bb3ef7259a820bf5e8ec4b4
NikolayLV <Nikolayd2002@gmail.com>
2023-08-16 12:56:54 +0300

    Create controllers.

--- vendor/wfm/Controller.php
+++ vendor/wfm/Controller.php
@@ -7,8 +7,19 @@ namespace wfm;
 abstract class Controller
 {
 
+    public array $data = [];
+    public array $meta = ['title' => '', 'description' => '', 'keywords' => ''];
+    public false|string $layout = '';
+    public string $view = '';
+    public string $model;
+
     public function __construct(public $route = []) {
 
+    }
+
+    public function getModel() {
+        $model = 'app\models\\' . $this->route['admin_prefix'] . $this->route['controller'];
+        $this->model = $model;
     }
 
 }
