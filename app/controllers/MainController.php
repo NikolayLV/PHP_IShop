c8672f216f07ca61a34bae6024caef95c3c9322c
NikolayLV <Nikolayd2002@gmail.com>
2023-08-17 13:00:18 +0300

    Create a Model to interact with the database.

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,6 +4,7 @@
 namespace app\controllers;
 
 
+use RedBeanPHP\R;
 use wfm\Controller;
 
 class MainController extends Controller
@@ -14,6 +15,8 @@ class MainController extends Controller
 
     public function indexAction() {
        // 'default' or 'ishop'
+        $names = R::findAll('name');
+        debug($names);
         $this->setMeta('Главная страница','Description','KeyWords');
         $this->layout = "ishop";
     }
