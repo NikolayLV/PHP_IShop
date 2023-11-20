f1c5369eba182119df49c31784b43db9a7ce9c52
Nikolay <Nikolayd2002@gmail.com>
2023-11-20 10:35:28 +0100

    Make blog page

--- app/views/Blogs/view.php
+++ app/views/Blogs/view.php
@@ -7,5 +7,5 @@
 <section>
 	<h1>There is a lot of blogs!</h1>
 	<p href="model">First blog</p>
-	<a class='nav-link' href='blog/model' style="white-space: nowrap;"><?php __('tpl_blog_link'); ?></a>
+	<a class='nav-link' href='blogs/model' style="white-space: nowrap;"><?php __('tpl_blog_link'); ?></a>
 </section>
